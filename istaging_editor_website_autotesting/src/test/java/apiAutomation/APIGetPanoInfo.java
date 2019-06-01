5b283eda92be2e01c32e2491055c8edf1d0b1cb4
Joy Shen <joy@staging.com.tw>
2019-06-01 11:12:58 +0800

    modify APIGetPanoInfo method(add Assert check by using TestNG)

--- istaging_editor_website_autotesting/src/test/java/apiAutomation/APIGetPanoInfo.java
+++ istaging_editor_website_autotesting/src/test/java/apiAutomation/APIGetPanoInfo.java
@@ -11,6 +11,7 @@ import org.apache.logging.log4j.Logger;
 import org.json.JSONException;
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -21,12 +22,11 @@ import com.relevantcodes.extentreports.LogStatus;
 import testSuitesLoginPage.JsLoginPageTest;
 
 public class APIGetPanoInfo {
-
-	public static void main(String[] args) throws Exception {
-		
+	
+	@Test
+	public void checkCubemapReady() throws JSONException, Exception {
 		APIGetPanoInfo pi = new APIGetPanoInfo();
 		pi.panoInfo();
-
 	}
 
 	public void panoInfo() throws JSONException, Exception {
@@ -36,12 +36,12 @@ public class APIGetPanoInfo {
 		APICreatePanoramas p = new APICreatePanoramas();
 		String panoId = p.createPanorama();
 
-		TimeUnit.SECONDS.sleep(30);
+		TimeUnit.SECONDS.sleep(20);
 
 		String checkurl = "https://vrcam-test-api.istaging.com/api/v2/panoramas/" + panoId;
 		URL connectto = new URL(checkurl);
 		HttpURLConnection conn = (HttpURLConnection) connectto.openConnection();
-		conn.setRequestMethod("GET");// optional default is GET
+		conn.setRequestMethod("GET"); // optional default is GET
 		conn.setRequestProperty("iStaging-Token", token);
 		conn.setRequestProperty("iStaging-Token-Type", "istaging");
 		conn.setRequestProperty("Content-Type", "application/json;charset=utf-8"); // 设置参数类型是json格式
@@ -73,17 +73,11 @@ public class APIGetPanoInfo {
 		br.close();
 		System.out.println(sb.toString());
 
-		// JsonParser parser = new JsonParser();
-		// JsonArray jsonArray = parser.parse(sb.toString()).getAsJsonArray();
-		// String cubemapReady =
-		// jsonArray.get(0).getAsJsonObject().get("cubemapReady").getAsString();
-		// return cubemapReady;
 
 		String isCubemapReady = cubemapReady(sb);
 		System.out.println("isCubemapReady return value is : " + isCubemapReady);
 		
-		Assert.assertTrue(Boolean.valueOf(isCubemapReady));
-		Assert.assertTrue(true);
+		Assert.assertEquals(isCubemapReady, "true");
 		
 
 	}
